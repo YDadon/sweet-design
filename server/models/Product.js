44e81752dc10a3d923a0e01f2bbeaf88d1d3d4b0
YDadon <yonatandadon20@gmail.com>
2025-11-09 12:40:26 +0200

    model folder

---
+++ server/models/Product.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    // Product name
+    name: {
+        type: String,
+        required: true,
+        trim: true,    
+    },
+
+    // Product description
+    description: {
+        type: String,
+        required: true,
+        // trim: true,
+    },
+
+    // Product category
+    category: {
+        type: String,
+        required: true,
+        enum: ['Events', 'Holidays', 'Kids', 'TeamGifts','Seasonal', 'Other'],
+        subCategories: {
+            type: [String],
+            default: [],
+        },
+    },
+
+    // Product price
+    imageUrl: [{
+        type: String,
+        required: true,
+    }],
+
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+
+    customFields: {
+        type: mongoose.Schema.Types.Mixed,
+        default: {},
+    },
+
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
